6b45fdb53302adac80ea7cb44ea28bf61c2b5609
DieselNiu <niu853273494@qq.com>
2022-03-24 20:26:26 +0800

    DONE: ready for Repeatability check

--- src/main/java/Args.java
+++ src/main/java/Args.java
@@ -2,6 +2,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Args {
 	public static <T> T parse(Class<T> optionsClass, String... args) {
@@ -17,21 +18,16 @@ public class Args {
 	}
 
 	private static Object parseOption(List<String> arguments, Parameter parameter) {
-		Object value = null;
 		Option option = parameter.getAnnotation(Option.class);
-		if (parameter.getType() == boolean.class) {
-			value = arguments.contains("-" + option.value());
-		}
-		if (parameter.getType() == int.class) {
-			int index = arguments.indexOf("-" + option.value());
-			value = Integer.parseInt(arguments.get(index + 1));
-		}
-
-		if (parameter.getType() == String.class) {
-			int index = arguments.indexOf("-" + option.value());
-			value = arguments.get(index + 1);
-		}
-		return value;
+		Class<?> type = parameter.getType();
+		OptionParser parser = PARSER.get(type);
+		return parser.parse(arguments, option);
 	}
+
+
+	private static Map<Class<?>, OptionParser> PARSER = Map.of(boolean.class, new BooleanParser(),
+		int.class, new IntOptionParser(),
+		String.class, new StringOptionParser());
+
 
 }
